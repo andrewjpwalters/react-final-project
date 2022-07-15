f85cbfcd66b598ce76947692536909e26ca6f5ad
Andrew Walters <andrewjpwalters@gmail.com>
2022-07-14 21:10:17 -0500

    add NavBar component and add reference in App.js

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <div>
+            <a>
+                Home
+            </a>
+            <a>
+                Log
+            </a>
+            <a>
+                Write
+            </a>
+        </div>
+    )
+}
+
+export default NavBar;
