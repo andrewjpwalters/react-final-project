52e03a54543c2828402b3fc5de03d311bce245cb
Andrew Walters <andrewjpwalters@gmail.com>
2022-07-14 21:17:50 -0500

    add Log component and import to App,js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from './NavBar.js'
+import Log from './Log'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Log />
     </div>
   );
 }
---
+++ src/Log.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Log() {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default Log;
