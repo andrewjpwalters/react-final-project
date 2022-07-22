6f732a6e54448d814b48a792bd6fd3e672715db2
Andrew Walters <andrewjpwalters@gmail.com>
2022-07-21 20:18:03 -0500

    add splash image to Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Image } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import image from './img/patrick-fore-0gkw_9fy0eQ-unsplash.jpg'
 
 function Home() {
     return (
-        <h1>Hello from Home</h1>
+        <>
+            <h1>Hello from Home</h1>
+            <Image fluid src={image} />
+        </>
     )
 }
 
